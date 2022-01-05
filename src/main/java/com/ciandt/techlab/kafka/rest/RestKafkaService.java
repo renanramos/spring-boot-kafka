03a1e91ec9ecab5ae525c6c4505b6dc9ad0aedfe
Renan Rodrigues Ramos (Ci&t) <renanramos@ciandt.com>
2022-01-05 13:41:57 -0300

    Alteracao de nome

--- src/main/java/com/ciandt/techlab/kafka/rest/RestKafkaService.java
+++ src/main/java/com/ciandt/techlab/kafka/rest/RestKafkaService.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class RestService {
+public class RestKafkaService {
 
 	private final StreamsBuilderFactoryBean factoryBean;
 
